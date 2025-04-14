3d1bfcab05c6aff9c12e174d5f72b9686aaf089b
ktosiekk <ktosiekk@o2.pl>
2025-04-14 20:24:35 +0200

    Chaning index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+    throw new Error('Root element not found. Ensure that index.html contains a <div id="root"></div>.');
+}
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
